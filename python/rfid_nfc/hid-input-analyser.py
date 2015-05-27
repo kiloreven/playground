dd866db70ddf513f37d391314ac9a0e9a2353452
Karl Fredrik Haugland <kfh@kaylee.caffe1.net>
2015-05-27 14:05:31 +0200

    added type-analysis, supporting hex and decimal

--- python/rfid_nfc/hid-input-analyser.py
+++ python/rfid_nfc/hid-input-analyser.py
@@ -1,13 +1,38 @@
 #! /bin/python
+import re
 
-def can_be_hex(source):
-    
+def find_type_and_cast_to_string(input):
+    if isinstance(input, (int, float)):
+        if isinstance(input, float):
+            input = int(input)
+
+        output = unicode(input)
+        output_type = 'dec'
+
+    elif isinstance(input, (str, unicode)):
+        if re.match(input.upper(), '[A-F]'):
+            output = re.sub(':', '', unicode(input.upper()))
+            output_type = 'hex'
+        elif not input.isdigit():
+            output = None
+            output_type = 'err' 
+        else:
+            output = unicode(input)
+            output_type = 'dec'
+    else:
+        output = None
+        output_type = 'err' 
+
+    return output, output_type
 
 
 def prompt():
-    user_input = raw_input('Scan a device on an NFC/RFID HID reader:  ')
+    user_input = raw_input('\nScan a device on an NFC/RFID HID reader:  ')
+
+    value, type = find_type_and_cast_to_string(user_input)
 
     print '\n== ANALYSIS =='
+    print 'Type: %s\nInput: %s\nWashed input: %s' % (type, user_input, value)
 
     prompt()
 
