3dc082824fa94fc8d76ed444396d1a58c66b3923
Karl Fredrik <karl.fredrik.haugland@gmail.com>
2015-05-27 12:27:17 +0200

    started actually writing the NFC/RFID-analyser

---
+++ python/rfid_nfc/hid-input-analyser.py
@@ -0,0 +1,14 @@
+#! /bin/python
+
+def can_be_hex(source):
+    
+
+
+def prompt():
+    user_input = raw_input('Scan a device on an NFC/RFID HID reader:  ')
+
+    print '\n== ANALYSIS =='
+
+    prompt()
+
+prompt()
